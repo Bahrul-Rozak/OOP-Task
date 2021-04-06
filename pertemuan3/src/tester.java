966526616f49e9701334b2c10dcf819811eeaa3a
Bahrul-Rozak <57394564+Bahrul-Rozak@users.noreply.github.com>
2021-04-06 16:37:01 +0700

    fixed bug

--- pertemuan3/src/tester.java
+++ pertemuan3/src/tester.java
@@ -1,9 +1,9 @@
 public class tester {
     public static void main(String[] args) {
-        // Persegi persegi = new Persegi(5);
-        // System.out.println("Data persegi saya ambil dari class Persegi");
-        // System.out.println("Luas " + persegi.hitungLuas());
-        // System.out.println("Panjang Sisi" + persegi.getSisi());
+        persegi persegi1 = new persegi();
+        System.out.println("Data persegi saya ambil dari class Persegi");
+        System.out.println("Luas " + persegi1.luas());
+        System.out.println("Panjang Sisi" + persegi1.keliling());
 
         PersegiPanjang pp = new PersegiPanjang();
         pp.setPanjang(5);
