fa1591cb2fbbfa220e1748cdcc743dc07bac072a
Bahrul-Rozak <57394564+Bahrul-Rozak@users.noreply.github.com>
2021-04-27 17:31:30 +0700

    add file :v

---
+++ tugas3/src/segitiga.java
@@ -0,0 +1,11 @@
+public class segitiga {
+    public static void main(String[] args) {
+        int x, y;
+        for (x = 1; x <= 6; x++) {
+            System.out.println("");
+            for (y = 1; y <= x; y++) {
+                System.out.print("*");
+            }
+        }
+    }
+}
